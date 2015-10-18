93126f45ec894d28481f5106a5f2586fd1138c9e
Cristina Alexa <cristina.alexa92@gmail.com>
2015-10-18 14:44:19 +0300

    Bugs fixed regarding DB configuration

--- app/__init__.py
+++ app/__init__.py
@@ -6,18 +6,16 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.sqlalchemy import SQLAlchemy 
 from flask.ext.login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
 app.secret_key = "secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/diana/projects/greentea/app.db'
-app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
 login_manager= LoginManager(app)
-login_manager.init_app(app)
-manager =  Manager(app)
-manager.add_command('db', MigrateCommand)
+
 
 Bootstrap(app)
+
 from app import views, models
 
--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+print basedir
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'data.sqlite')
